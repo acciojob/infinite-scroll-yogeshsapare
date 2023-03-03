8687abca58f297c819ed72ad4590d8f30355fcfa
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-03-03 14:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+const container = document.querySelector('.container');
 
+const URL = 'https://dog.ceo/api/breeds/image/random'
+
+// get the images
+
+function loadImages(numImages = 10){
+   let i=0;
+    while(i < numImages){
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response=>response.json())
+    .then(data=>{
+    // console.log(data.message)
+    const img =  document.createElement('img');
+    img.src = `${data.message}`
+    container.appendChild(img)
+    })
+    i++;
+    }   
+    }
+
+loadImages();
